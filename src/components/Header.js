33a76907e2981abb37df3bf7cee551bbfccd902d
Olga <minaeva9@gmail.com>
2024-06-02 16:36:52 -0400

    header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <img src="logo.png" alt="quiz logo" />
+      <h1>Quysy</h1>
+    </header>
+  );
+}
+
+export default Header;
