6ffe97d34838f06daecc779c0329d4cd603df73c
Olga <minaeva9@gmail.com>
2024-06-02 17:02:45 -0400

    reducer setCategories

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,46 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import Main from "./Main";
+import StartGame from "./StartGame";
+
+//define initial state
+const initialState = {
+  categories: [],
+  selectCategory: null,
+};
+
+//define reducer function
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_CATEGORIES":
+      return {
+        ...state,
+        categories: action.payload,
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //fetch category of quiz and mount on load
+  useEffect(function () {
+    fetch("https://opentdb.com/api_category.php")
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({ type: "SET_CATEGORIES", payload: data.trivia_categories })
+      )
+      .catch((err) => console.error("Error fetching trivia categories:"));
+  }, []);
+
   return (
     <div className="app">
       <Header />
+      <Main>
+        <StartGame />
+      </Main>
     </div>
   );
 }
---
+++ src/components/StartGame.js
@@ -0,0 +1,10 @@
+function StartGame() {
+  return (
+    <div className="start">
+      <h2>Are you ready to challenge yourself?</h2>
+      <p>Choose category</p>
+    </div>
+  );
+}
+
+export default StartGame;
