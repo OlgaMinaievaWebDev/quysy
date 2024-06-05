824f31baac9c99a74c93121939765b52ec286869
Olga <minaeva9@gmail.com>
2024-06-05 13:30:22 -0400

    quizsetup component

--- src/components/App.js
+++ src/components/App.js
@@ -1,77 +1,14 @@
-import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
-import CategorySelector from "./CategorySelector";
-import Difficulty from "./Difficulty";
-import StartGame from "./StartGame";
-
-//define initial state
-const initialState = {
-  categories: [],
-  selectedCategory: null,
-  difficulties: [],
-  selectedDifficulties: null,
-  questions: [],
-  loading: false,
-  error: null,
-};
-
-//define reducer function
-function reducer(state, action) {
-  switch (action.type) {
-    case "FETCH_CATEGORIES":
-      return {
-        ...state,
-        categories: action.payload,
-      };
-    case "SET_DIFFICULTIES":
-      return {
-        ...state,
-        difficulties: action.payload,
-      };
-    case "SELECT_CATEGORY":
-      return {
-        ...state,
-        selectedCategory: action.payload,
-      };
-    case "SET_DIFFICULTY":
-      return {
-        ...state,
-        selectedDifficulty: action.payload,
-      };
-    default:
-      return state;
-  }
-}
+import QuizSetup from "./QuizSetup";
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  //fetch category of quiz and mount on load
-  useEffect(function () {
-    fetch("https://opentdb.com/api_category.php")
-      .then((res) => res.json())
-      .then((data) =>
-        dispatch({ type: "FETCH_CATEGORIES", payload: data.trivia_categories })
-      )
-      .catch((err) => console.error("Error fetching trivia categories:", err));
-  }, []);
-
-  //fetch difficulty of quiz
-  useEffect(function () {
-    const difficulties = ["easy", "medium", "hard"];
-    dispatch({ type: "SET_DIFFICULTIES", payload: difficulties });
-  }, []);
-
   //return
   return (
     <div className="app">
       <Header />
-      <Main>
-        <CategorySelector categories={state.categories} dispatch={dispatch} />
-        <Difficulty difficulties={state.difficulties} dispatch={dispatch} />
-        <StartGame selectedCategory={state.selectedCategory} />
-      </Main>
+      <QuizSetup />
+      <Main></Main>
     </div>
   );
 }
---
+++ src/components/QuizSetup.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+
+function QuizSetup() {
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState("");
+  const [numberQuestions, setNumberQuestions] = useState(10);
+  const [difficulty, setDifficulty] = useState([]);
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api_category.php")
+      .then((res) => res.json())
+      .then((data) => setCategories(data.trivia_categories))
+      .catch((error) => console.log("There is an error", error));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(category);
+    console.log(numberQuestions);
+    console.log(difficulty);
+  };
+  const handleCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const handleDifficulty = (e) => {
+    setDifficulty(e.target.value);
+  };
+
+  const handleNumQuestions = (e) => {
+    setNumberQuestions(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="category">Choose Category</label>
+      <select id="category" value={category} onChange={handleCategory}>
+        <option value="">---Select Category---</option>
+        {categories.map((category) => (
+          <option value={category.id} key={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </select>
+      <div>
+        <label htmlFor="numQuestions">Choose Amount of Questions</label>
+        <input
+          type="number"
+          id="numQuestions"
+          min={10}
+          max={50}
+          value={numberQuestions}
+          onChange={handleNumQuestions}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="difficulty">Choose Difficulty</label>
+        <select value={difficulty} onChange={handleDifficulty}>
+          {["easy", "medium", "hard"].map((option) => (
+            <option value={option} key={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button type="submit">Start Quizy</button>
+    </form>
+  );
+}
+
+export default QuizSetup;
